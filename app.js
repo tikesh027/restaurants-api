5c413ba0d902d386804e2b5866a2cc3974a3369e
tikesh027 <tikeshsingh027@yahoo.com>
2022-11-26 16:56:28 +0530

    Connection setup with database using mongoose

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+const MONGODB_URI = 'mongodb+srv://tikesh027:tikesh@cluster0.xgb8plp.mongodb.net/?retryWrites=true&w=majority';
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.listen(3000);
+mongoose.connect(MONGODB_URI)
+    .then(()=>{
+        app.listen(3000);
+        console.log('Connected');
+    }).catch((error) => {
+        console.log(error)
+    });
