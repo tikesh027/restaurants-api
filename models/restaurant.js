d96299a255c34c000bda4909826ade74b5fc2d99
tikesh027 <tikeshsingh027@yahoo.com>
2022-11-26 16:53:49 +0530

    Added Restaurant schema

---
+++ models/restaurant.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const restaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    imageURL: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Restaurant', restaurantSchema);
