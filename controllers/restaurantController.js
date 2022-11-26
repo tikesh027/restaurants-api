c18f11c17a4dcff7512298a3e6ab2222262afbcc
tikesh027 <tikeshsingh027@yahoo.com>
2022-11-26 17:03:24 +0530

    Created API to Fetch All Restaurants

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -17,3 +17,16 @@ exports.addRestaurant = (req, res, next) => {
         res.status(500).send(error.message);
     });
 }
+
+exports.getAllRestaurants = (req, res, next) => {
+    Restaurant.find().then((result) => {
+        const responseBody = {
+            restaurants: result,
+            message: 'Restaurants fetched successfully.'
+        }
+        console.log(responseBody);
+        res.status(200).send(responseBody);
+    }).catch((error) => {
+        res.status(500).send(error.message);
+    });
+}
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const restaurantController = require('../controllers/restaurantController');
 
 router.post('/api/restaurant/add', restaurantController.addRestaurant);
-
+router.get('/api/restaurant/', restaurantController.getAllRestaurants);
 module.exports = router;
